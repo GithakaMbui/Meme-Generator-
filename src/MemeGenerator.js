6355333b4b98b708854a75fcb730c15059fdb979
Githaka <githakamaru@gmail.com>
2020-02-16 11:55:20 +0300

    made an api call to get images for my meme project

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -3,7 +3,23 @@ import React, { Component } from "react"
 class MemeGenerator extends Component {
     constructor() {
         super()
-        this.state = {}
+        this.state = {
+            topText: "",
+            bottomText: "",
+            randomImg: "http://i.imgflip.com/1bij.jpg",
+            allMemeImgs: []
+        }
+    }
+
+    //We will be using an APi that provide a bunch of meme images
+    componentDidMount() {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(response => response.json())
+            .then(response => {
+                const { memes } = response.data
+                console.log(memes[0])
+                this.setState({ allMemeImgs: memes })
+            })
     }
 
     render() {
